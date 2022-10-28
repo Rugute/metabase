d99179152b089d621c9913ee5ae9c36e1fdc8e86
Rugute <rugute@gmail.com>
2022-10-28 13:26:53 +0300

    Added initial settup for metabase

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # metabase
+. Open where you have downloaded the application.
+. run sudo bash setup_script.sh or sh setup_script.sh
+. Wait for the application to install
+####################
+. Visit the application on the browser using http://localhost:3000
+
---
+++ setup_script.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#read -p "Enter mySQL Password: " x
+#echo "Welcome ${x}!"
+mysql_user="root"
+mysql_password=${x}
+mysql_base_database="metabase"
+mysql_information_schema_database="information_schema"
+echo "Creating Metabase user"
+#mysql --user=${mysql_user} --password=${mysql_password} ${mysql_base_database} -e "CREATE DATABASE metabase;"
+#mysql --user=${mysql_user} --password=${mysql_password} ${mysql_base_database} -e "GRANT ALL PRIVILEGES ON metabase.* TO metabase@localhost IDENTIFIED BY test123"
+#mysql --user=${mysql_user} --password=${mysql_password} ${mysql_base_database} -e "FLUSH PRIVILEGES;"
+#mysql --user=${mysql_user} --password=${mysql_password} ${mysql_base_database} -e "QUIT" 
+
+echo "User creation completed successfully"
+
+sudo ansible-playbook install_metabse.yml
+
+
+    
